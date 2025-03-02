ef24e6f078895684cf9aa905db4da300330534c6
Asmita362 <asmitapawar141@gmail.com>
2025-03-02 17:24:59 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,27 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import SignupPage from "./Signup"; // ✅ Ensure correct import
+import Register from "./Register"; 
 import Login from "./Login";
 import Home from "./Home";
-
-
+import Goals from "./Goals";
+import Budget from "./Budget"
+import Reports from "./Reports";
+// import Dashboard from "./Home";
+import HomePage from "./Home";
+import Transaction from "./Transaction";
 function App() {
   return (
     <BrowserRouter>
       <Routes>  
         <Route path="/" element={<Home />} />  
-        <Route path="/signup" element={<SignupPage />} />  
+        <Route path="/register" element={<Register />} />  
         <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/goals" element={<Goals />} />
+        <Route path="/budget" element={<Budget />} />
+        <Route path="/report" element={<Reports />} />
+        <Route path="/transaction" element={<Transaction />} />
+        {/* <Route path="/home" element={<HomePage />} /> */}
         
-
       </Routes>
     </BrowserRouter>
   );
